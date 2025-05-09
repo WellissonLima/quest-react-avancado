b9ec34662843c213347cb1dc503015c03f20a8b2
Wellisson Lima <wellissonboanerges@gmail.com>
2025-05-09 19:55:15 -0300

    Modificação no hook para buscar os dez primeiros pokémons no lugar de buscar dez aleatórios

--- src/hooks/useFetchPokemons.jsx
+++ src/hooks/useFetchPokemons.jsx
@@ -1,42 +1,51 @@
 import { useEffect, useState } from "react";
 
-export const useFetchPokemons = (count = 10) => {
+export const useFetchPokemons = () => {
     const [pokemons, setPokemons] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [offset, setOffset] = useState(0);
+    const limit = 10;
+
+
+
+    const fetchPokemons = async (currentOffset = 0) => {
+        setLoading(true);
+
+        try {
+            //const pokemonsList = [];
+
+
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${currentOffset}&limit${limit}`);
+            if (!response.ok) throw new Error("Erro ao buscar Pokémon");
+
+            const data = await response.json();
+
+            const detailsPromises = data.results.map(async (pokemon) => {
+                const res = await fetch(pokemon.url);
+                const details = await res.json();
+                const image = data.sprites?.other?.dream_world?.front_default;
+                return {
+                    name: details.name,
+                    image
+                };
+            });
+
+            const newPokemons = await Promise.all(detailsPromises);
+            setPokemons((prev) => [...prev, ...newPokemons]);
+            setOffset((prev) => prev + limit);
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        const fetchPokemons = async () => {
-            try {
-                const pokemonsList = [];
-
-                while (pokemonsList.length < count) {
-                    const randomNumber = Math.floor(Math.random() * 800);
-                    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
-                    if (!response.ok) throw new Error("Erro ao buscar Pokémon");
-
-                    const data = await response.json();
-                    //console.log(data);
-                    const image = data.sprites?.other?.dream_world?.front_default;
-
-                    if (image) {
-                        pokemonsList.push({
-                            name: data.name,
-                            image
-                        });
-                    }
-                }
-
-                setPokemons(pokemonsList);
-            } catch (err) {
-                setError(err.message);
-            } finally {
-                setLoading(false);
-            }
-        };
-
         fetchPokemons();
-    }, [count]);
+    }, []);
 
-    return { pokemons, loading, error };
+    const loadMorePokemons = () => fetchPokemons(offset);
+
+    return { pokemons, loading, error, loadMorePokemons };
 };
