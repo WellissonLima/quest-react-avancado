ec81c1f89f19275a03b27ad53c775f8636540e73
Wellisson Lima <wellissonboanerges@gmail.com>
2025-05-28 16:50:03 -0300

    Edição dos estilos para a página de e detalhe do pokémon

--- src/components/pokemon/PokemonDetails.styles.js
+++ src/components/pokemon/PokemonDetails.styles.js
@@ -1,22 +1,43 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  gap: 32px;
   padding: 32px;
   max-width: 900px;
   margin: 0 auto;
+  background-color: rgba(220, 220, 220, 0.8);
+  border-radius: 16px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const LeftColumn = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const RightColumn = styled.div`
+  flex: 2;
 `;
 
 export const Title = styled.h1`
   text-transform: capitalize;
   text-align: center;
   font-size: 2rem;
+  margin-top: 12px;
 `;
 
 export const Image = styled.img`
   display: block;
   margin: 20px auto;
-  width: 150px;
-  height: 150px;
+  min-width: 150px;
+  min-height: 150px;
   object-fit: contain;
 `;
 
